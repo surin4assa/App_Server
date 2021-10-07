ba7f38794f6ae64aeb1f547118931eece458f456
SurinA <13841818+surin4assa@users.noreply.github.com>
2021-10-07 12:17:52 -0500

    changes

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using System.Security.Crytography
+using System.Security.Cryptography;
 using API.Entities;
 using API.Data;
+using System.Text;
+using System;
 
 namespace API.Controllers
 {
@@ -15,16 +17,19 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(string username, string password, string firstname, string lastname)
         {
-            using var hmac = new HMACSHA512();
+            using var hmac = new HMACSHA512(); //using keyword for disposable object
 
             var user = new AppUser
             {
                 UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-                PasswordSalt = hmac.key;
-            }
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key,
+                FirstName = firstname,
+                LastName = lastname,
+                CreateDT = DateTime.Now
+            };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
