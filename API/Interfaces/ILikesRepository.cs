00108e76afd221138d4c17d55263b0a15d049458
SurinA <13841818+surin4assa@users.noreply.github.com>
2021-10-12 02:37:44 -0500

    ILikeRepo removed unused refs

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -1,9 +1,6 @@
 ﻿using API.DTOs;
 using API.Entities;
 using API.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Interfaces
